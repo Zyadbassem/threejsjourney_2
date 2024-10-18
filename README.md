1e60a342cdfbdf0c6dd2507537702a08f5a7a65e
zyad <zyadbassem9090@gmail.com>
2024-10-18 17:43:07 +0300

    house

--- README.md
+++ README.md
@@ -119,3 +119,67 @@ floor.rotateX(-Math.PI * 0.5);
 ```
 
 now you should have a floor, feel free to remove the sphere
+
+### House
+
+so now let's create our house first we will create a group then add house compenents to it like walls and roof and door
+
+```js
+/**
+ * House
+ */
+
+// House Group
+const house = new THREE.Group();
+scene.add(house);
+
+// walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+walls.position.y = 2.5 / 2;
+house.add(walls);
+
+// roof
+const roof = new THREE.Mesh(
+  new THREE.ConeGeometry(3.5, 1.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+roof.position.y = 3.26;
+roof.rotateY(Math.PI * 0.25);
+house.add(roof);
+
+// door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2.2, 2.2),
+  new THREE.MeshStandardMaterial()
+);
+door.position.z = 2.0001;
+door.position.y = 1.1;
+house.add(door);
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial();
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+```
+
+and this way we built our house you can change the color of the door so you can see it and all these things we learned before so if you find anything you cant understand you can go back to its lesson
--- src/main.js
+++ src/main.js
@@ -22,14 +22,69 @@ const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
   new THREE.MeshStandardMaterial()
 );
+scene.add(floor);
 
 // Rotate the floor
 floor.rotateX(-Math.PI * 0.5);
 
-// Adding them to the scene
-scene.add(floor);
+/**
+ * House
+ */
 
-// LIGHTS
+// House Group
+const house = new THREE.Group();
+scene.add(house);
+
+// walls
+const walls = new THREE.Mesh(
+  new THREE.BoxGeometry(4, 2.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+walls.position.y = 2.5 / 2;
+house.add(walls);
+
+// roof
+const roof = new THREE.Mesh(
+  new THREE.ConeGeometry(3.5, 1.5, 4),
+  new THREE.MeshStandardMaterial()
+);
+roof.position.y = 3.26;
+roof.rotateY(Math.PI * 0.25);
+house.add(roof);
+
+// door
+const door = new THREE.Mesh(
+  new THREE.PlaneGeometry(2.2, 2.2),
+  new THREE.MeshStandardMaterial()
+);
+door.position.z = 2.0001;
+door.position.y = 1.1;
+house.add(door);
+
+// Bushes
+const bushGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushMaterial = new THREE.MeshStandardMaterial();
+
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush1.scale.set(0.5, 0.5, 0.5);
+bush1.position.set(0.8, 0.2, 2.2);
+
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush2.scale.set(0.25, 0.25, 0.25);
+bush2.position.set(1.4, 0.1, 2.1);
+
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush3.scale.set(0.4, 0.4, 0.4);
+bush3.position.set(-0.8, 0.1, 2.2);
+
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial);
+bush4.scale.set(0.15, 0.15, 0.15);
+bush4.position.set(-1, 0.05, 2.6);
+
+house.add(bush1, bush2, bush3, bush4);
+/**
+ * LIGHTS
+ */
 
 // Ambient
 const ambient = new THREE.AmbientLight(0xffffff, 0.7);
@@ -39,7 +94,7 @@ scene.add(ambient);
 const directional = new THREE.DirectionalLight(0xffffff, 0.6);
 directional.position.x = 4;
 directional.position.y = 4;
-directional.position.z = 4;
+directional.position.z = -4;
 scene.add(directional);
 
 // Create a camera
@@ -51,7 +106,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.setZ(5);
 camera.position.setY(3);
-camera.position.setX(2);
+camera.position.setX(4);
 scene.add(camera);
 
 //Controls
